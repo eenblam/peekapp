61edd7bbeb34c39f9563ec4aaa3a712cadf3bf0b
Ben Elam <ben.a.elam@gmail.com>
2016-10-18 20:32:48 -0500

    Add IP blacklisting and main loop

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ import fileinput
 from sys import stdin, stdout
 
 @click.group()
-@click.option('--signatures', '-s', type=click.File('rb'))
-@click.option('--blacklists', '-b', type=click.File('rb'))
+@click.option('--signatures', '-s', type=click.File('rb'),
+        help='File listing TCP/UDP payload signatures')
+@click.option('--blacklists', '-b', type=click.File('rb'),
+        help='File listing strings triggering an alert when present in URls')
+@click.option('--ip_blacklist', '-i', type=click.File('rb'),
+        help='File listing IPs for which any traffic should trigger an alert')
 @click.argument('log', default='peekapp.log', type=click.File('wb'))
-def cli(signatures, blacklists, log):
+def cli(signatures, blacklists, ip_blacklist, log):
     """
-    peekapp is a PCAP-monitoring IDS layer.
+    peekapp is a PCAP-monitoring IDS layer, which records traffic in LOG.
     """
     pass
 
@@ -31,3 +35,6 @@ def iface(interface):
     Monitor a specific network interface in promiscuous mode
     """
     pass
+
+if __name__ == '__main__':
+    cli()
