940f6976355e4daa605956cb59c1f2556940ccaa
Ben Elam <ben.a.elam@gmail.com>
2016-10-24 23:44:00 -0500

    Move classification code into classifiers.py

---
+++ classifiers.py
@@ -0,0 +1,18 @@
+# classifiers.py
+
+# Message formatters / classifiers
+from scapy.all import NoPayload
+from collections import namedtuple
+#TODO Add 'rule' field to MSG to identify what was matched on
+MSG = namedtuple('msg', ['timestamp', 'src', 'dst', 'traffic_type',
+                         'rule', 'payload', 'pkt'])
+
+def classify_pkt(pkt, traffic_type, rule=None, payload=NoPayload):
+    msg = MSG(time=pkt.time,
+            src=pkt[IP].src,
+            dst=pkt[IP].dst,
+            traffic_type=traffic_type,
+            rule=rule,
+            payload=payload,
+            pkt=pkt)
+    return msg
--- pipes.py
+++ pipes.py
@@ -1,21 +1,5 @@
 from scapy.all import (Drain, Sink, NoPayload)
 
-# Message formatters / classifiers
-from collections import namedtuple
-#TODO Add 'rule' field to MSG to identify what was matched on
-MSG = namedtuple('msg', ['timestamp', 'src', 'dst', 'type',
-                         'payload', 'pkt'])
-
-def classify_pkt(pkt, traffic_type, rule=None, payload):
-    msg = MSG(time=pkt.time,
-            src=pkt[IP].src,
-            dst=pkt[IP].dst,
-            type=traffic_type,
-            rule=rule,
-            payload=payload,
-            pkt=pkt)
-    return msg
-
 # Drains
 class FilterDrain(Drain):
     '''Drop packets that fail filter'''
